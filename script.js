0473bf177e07ee8b6d624d98124c38822c43b7c0
Marty <477660@student.saxion.nl>
2023-06-24 02:07:54 +0200

    Added script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+let link = "https://www.youtube.com"
+document.getElementById("link").href = link
+
+const elem = document.createElement("p");
+elem.classList.add("colored"); // Add class to elem
+elem.appendChild(document.createTextNode("Bla"));
+document.getElementById("main").appendChild(elem);
+
+function changeText(elem) {
+    elem.innerHTML = "Thank you!";
+}
+
+document.getElementById("change")
+    .addEventListener("click", function () {
+            changeText(this);
+        }
+    );
