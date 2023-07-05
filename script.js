f9011eebc26a8c2c2e1b1cccd0413f1e064885e4
Marty <477660@student.saxion.nl>
2023-07-05 16:15:30 +0200

    Added loads of styling

--- script.js
+++ script.js
@@ -17,4 +17,70 @@ document.getElementById("change")
     );
 
  */
+let baseFetchUrl = 'http://localhost:3000'
+async function getBrands() {
+    try {
+        const response = await fetch(baseFetchUrl + '/car-brands');
+        const data = await response.json();
+        console.log(data);
+
+    } catch (e) {
+        console.error(e)
+    }
+}
+
+
+async function addBrand(carBrand){
+    try{
+        const response = await fetch(baseFetchUrl + '/car-brands', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(carBrand)
+        })
+        const data = await response.json();
+        console.log(data);
+    } catch (e){
+        console.log(e)
+    }
+}
+const brands = getBrands();
+for(const i in brands){
+    console.log(brands[i])
+
+}
+
+addBrand({
+    carModel: "nevera",
+    bouwjaar: 2022,
+    price: 3000000,
+    horsepower: 1914,
+    isEV: true
+}).then(console.log);
+
+
+/*
+ function addCarBrand(newCarBrand) {
+    fetch(baseFetchUrl + '/car-brands', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newCarBrand)
+    }).then(response => response.json())
+        .then(data => console.log(data));
+}
+
+
+
+function getCarBrands() {
+    fetch(baseFetchUrl + '/car-brands')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+*/
+
+
+
 
